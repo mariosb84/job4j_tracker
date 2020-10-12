9cfd277fd2c24fa8a816dd14fcb94aa3739fca65
Sergei Baranov <mariosb84@mail.ru>
2020-10-12 18:03:38 +0300

    6. Изменить программу Tracker из 2-го модуля [#328777]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,15 +1,21 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    //private final Item[] items = new Item[100];                                                       replace to List
+    List<Item> items = new ArrayList<Item>();
     private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        //items[size++] = item;                                                                         replace to List
+        //items.set(size++, item);                                                                      replace to List
+        items.add(size++, item);
         return item;
     }
 
@@ -28,7 +34,8 @@ public class Tracker {
         /* Находим индекс */
         int index = indexOf(id);
         /* Если индекс найден возвращаем item, иначе null */
-        return index != -1 ? items[index] : null;
+       // return index != -1 ? items[index] : null;                                                     replace to List
+        return index != -1 ? items.get(index) : null;
     }
     /*public Item[] findAll() {
 Item[] itemWithoutNull = new Item[items.length];
@@ -42,25 +49,33 @@ Item[] itemWithoutNull = new Item[items.length];
         }
         return Arrays.copyOf(itemWithoutNull, size);
     }*/
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+   // public Item[] findAll() {                                                                         replace to List      List<Item> items = new ArrayList<Item>();
+        public List<Item>  findAll() {
+       // return Arrays.copyOf(items, size);                                                            replace to List
+        return List.copyOf(items);
     }
-    public Item[] findByName(String key) {
-        Item[] itemWithKey = new Item[items.length];
+   // public Item[] findByName(String key) {                                                            replace to List
+        public List<Item>  findByName(String key) {
+       // Item[] itemWithKey = new Item[items.length];                                                  replace to List
+       // Item[] itemWithKey = new Item[items.size()];                                                  replace to List
+            List<Item> itemWithKey = new ArrayList<Item>();
         int size = 0;
         for (int i = 0; i < this.size; i++) {
-            Item item = items[i];
+           // Item item = items[i];                                                                     replace to List
+            Item item = items.get(i);
             if (item.getName().equals(key)) {
-                itemWithKey[size] = item;
+               // itemWithKey[size] = item;                                                              replace to List            ДОДЕЛАТЬ!!!!!!!!!
+               // itemWithKey.get() = item;
                 size++;
             }
         }
-        return Arrays.copyOf(itemWithKey, size);
+        return List.copyOf(itemWithKey);
     }
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+            //if (items[index].getId() == id) {                                                         replace to List
+                if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
             }
@@ -71,7 +86,8 @@ Item[] itemWithoutNull = new Item[items.length];
         boolean replaced = false;
         int index = indexOf(id);
         if (index != -1) {
-        items[index] = item;
+       // items[index] = item;                                                                          replace to List
+            items.set(index, item);
         item.setId(id);
             replaced = true;
         }
@@ -81,8 +97,10 @@ Item[] itemWithoutNull = new Item[items.length];
         boolean deleted = false;
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items,index + 1, items, index, size - index);
-            items[size - 1] = null;
+            //System.arraycopy(items,index + 1, items, index, size - index);                            replace to List
+            System.arraycopy(items,index + 1, items, index, size - index);                                          /// ДОДЕЛАТЬ!!!!!!!!!
+            //items[size - 1] = null;                                                                   replace to List
+            items.set(size - 1, null);
             size--;
             deleted = true;
         }
